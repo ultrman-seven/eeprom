093e5c6f13b26db1f9d8cc5d09b82ad4639c6a3f
ultrman-seven <54103434+ultrman-seven@users.noreply.github.com>
2020-10-03 21:58:00 +0800

    debug
    iic.c
    modified iicSendByte, iicReadByte, read24C02
    test.c
    test functions in iic.c step by step
    show the test result in displayer

--- iic.c
+++ iic.c
@@ -21,56 +21,53 @@ void Write24C02(un8 write, un8 address)
 
 /*
 读出过程
-	 单片机先发送该器件的7位地址码和写方向位“0”（“伪写”），
-发送完后释放SDA线并在SCL线上产生第9个时钟信号。被选中的存储器器件在确认是自己的地址后，
-在SDA线上产生一个应答信号作为回应。
-
-然后，再发一个字节的要读出器件的存储区的首地址，收到应答后，
-单片机要重复一次起始信号并发出器件地址和读方向位（“1”），收到器件应答后就可以读出数据字节，
+S1.单片机发送该器件的7位地址码和写0（“伪写”），发送完后释放SDA线并在SCL线上产生第9个时钟信号。
+被选中的存储器器件在确认是自己的地址后，在SDA线上产生一个应答信号作为回应。
+S2.然后，再发一个字节的要读出器件的存储区的首地址，收到应答后，
+重复1次起始信号并发送器件地址和读1，收到器件应答后就可以读出数据字节，
 每读出一个字节，单片机都要回复应答信号。当最后一个字节数据读完后，单片机应返回以“非应答”（高电平），
 并发出终止信号以结束读出操作。
 */
 un8 Read24C02(un8 address)
 {
+	un8 result;
 	iicStart();
 	iicSendByte(0xa0);
-	delay(DELAY_10us);//while (SDA);
+	//delay(DELAY_10us);//while (SDA);
 	iicSendByte(address);
-	delay(DELAY_10us);//while (SDA);
+	//delay(DELAY_10us);//while (SDA);
 	iicStart();
 	iicSendByte(0xa1);
+	result = iicReadByte();
 	iicEnd();
-	return iicReadByte();
+	return result;
 }
 
 /*
  I2C总线进行数据传送时，时钟信号为高电平期间，数据线上的数据必须保持稳定，
  只有在时钟线上的信号为低电平期间，数据线上的高电平或低电平状态才允许变化。
 */
-
 un8 iicSendByte(un8 send)
 {
 	un8 count = 8;
 	un16 maxWait = 1000;
+	SCL = 0;
+	delay(DELAY_10us);
 	while (count--)
 	{
-		SCL = 0;
 		SDA = send >> 7;
 		send = send << 1;
 		delay(DELAY_10us);
 		SCL = 1;
 		delay(DELAY_10us);
 		SCL = 0;
+		delay(DELAY_10us);
 		SDA = 1;
 	}
 	SCL = 1;
-	delay(DELAY_10us);
 	while (SDA)
 		if (!maxWait--)
-		{
-			SCL = 0;
-			return 0;
-		}
+			return SCL = 0;
 	SCL = 0;
 	return 1;
 }
@@ -87,6 +84,11 @@ un8 iicReadByte(void)
 		result |= SDA;
 		SCL = 0;
 	}
+	delay(DELAY_10us);
+	SCL = 1;
+	delay(DELAY_10us);
+	SCL = 0;
+	delay(DELAY_10us);
 	return result;
 }
 
--- test.c
+++ test.c
@@ -1,13 +1,51 @@
 #include"iic.h"
-#define ADDRESS 2
+#define ADDRESS 1
+#define GAP delay(50000);P0 = 0x80;delay(50000);
 un8 dis_num[] = { 0x3f,0x06, 0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,/*0-9*/
 							0x77,0x7c,0x58, 0x5e, 0x79,0x71,/*a-f*/		0x80/*.*/ };
 void main()
 {
-    un8 num;
-    Write24C02(0x08,ADDRESS);
-    num = Read24C02(ADDRESS);
-    P0 = dis_num[num];
+	un8 result, count;
+	
+	iicStart();
+	P0 = dis_num[iicSendByte(0xa0)];
+	GAP
+	P0 = dis_num[iicSendByte(ADDRESS)+1];
+	GAP
+	P0 = dis_num[iicSendByte(0x04)+2];
+	GAP
+	//iicEnd();
+	
+	iicStart();
+	P0 = dis_num[iicSendByte(0xa0)];
+	GAP
+	P0 = dis_num[iicSendByte(ADDRESS)+1];
+	GAP
+	iicStart();
+	P0 = dis_num[iicSendByte(0xa1)+2];
+	GAP
+	//P0 = dis_num[iicReadByte()];
+	
+	for (result = count = SCL = 0; count < 8; count++)
+	{
+		delay(DELAY_10us);
+		SCL = 1;
+		delay(DELAY_10us);
+		result <<= 1;
+		result |= SDA;
+		P0 = dis_num[SDA];
+		GAP
+		SCL = 0;
+	}
+	GAP
+	P0 = dis_num[result];
+	GAP
+	iicEnd();
+	
+//    un8 num;
+//    Write24C02(0x03,ADDRESS);
+//    num = Read24C02(ADDRESS);
+//    P0 = dis_num[num];
     while (1)
     ;
 }
